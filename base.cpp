f03ab3df72bfe2b7d8b42ef07f96f6db4a68cd5d
lizhuo <1102267073@qq.com>
2019-12-04 22:44:15 +0800

    add the game description

--- base.cpp
+++ base.cpp
@@ -193,7 +193,7 @@ int main() {
 		char s=getch();
 		switch(s)
 		{	case '3':	exit(0);break;//退出游戏 
-			case '2':	break;//我还没想好 
+			case '2':	intro();break;//游戏介绍 
 			case '1':	start_game();	break; //开始游戏
 			default:	continue; //防止你按错键 导致无法进入游戏 
 		}
--- front.cpp
+++ front.cpp
@@ -2,7 +2,7 @@
 #include "global_variable.cpp"
 #include "front_end.h"
 #include <Windows.h>
-
+#include <conio.h>
 //int Map[set.map_height+1][set.map_width+1]={0,};
 int times = 0;
 int py;		//在Map中player位置为 (player.x, py) 
@@ -81,4 +81,50 @@ void gameover(struct settings* set,struct player* pla)
 	SetConsoleActiveScreenBuffer(hOutBuf);
 	//设置新的缓冲区为活动显示缓冲
 }
+
+void intro()
+{
+	char s[13][60]={"Description"," 把红旗插上无穷的高峰！","向上跳跃，不要坠入虚空！","Howto play ","用方向键操纵小人左右移动，当小人落在跳板上时会向上弹起。","左移： A 或 ←","右移： D 或 →","Main Contributors","@lhy ：Backend Developer","@lz ：Frontend Developer","@Jonbgua ：Program Manager, Software Architect"};
+	HANDLE hOut;
+	hOut = CreateConsoleScreenBuffer(
+        GENERIC_WRITE,//定义进程可以往缓冲区写数据
+        FILE_SHARE_WRITE,//定义缓冲区可共享写权限
+        NULL,
+        CONSOLE_TEXTMODE_BUFFER,
+        NULL
+    );
+	CONSOLE_CURSOR_INFO cci;
+    cci.bVisible = 0;
+    cci.dwSize = 1;
+    SetConsoleCursorInfo(hOut, &cci);
+    coord.X = 3;
+	coord.Y = 3;
+    WriteConsoleOutputCharacterA(hOut, s[0], 50, coord, &bytes);
+    coord.Y = 5;
+    WriteConsoleOutputCharacterA(hOut, s[1], 50, coord, &bytes);
+    coord.Y = 6;
+    WriteConsoleOutputCharacterA(hOut, s[2], 50, coord, &bytes);
+    coord.Y = 10;
+    WriteConsoleOutputCharacterA(hOut, s[3], 50, coord, &bytes);
+    coord.Y = 12;
+    WriteConsoleOutputCharacterA(hOut, s[4], 50, coord, &bytes);
+    coord.Y = 13;
+    WriteConsoleOutputCharacterA(hOut, s[5], 50, coord, &bytes);
+    coord.Y = 14;
+    WriteConsoleOutputCharacterA(hOut, s[6], 50, coord, &bytes);
+    coord.Y = 18;
+    WriteConsoleOutputCharacterA(hOut, s[7], 50, coord, &bytes);
+    coord.Y = 20;
+    WriteConsoleOutputCharacterA(hOut, s[8], 50, coord, &bytes);
+    coord.Y = 21;
+    WriteConsoleOutputCharacterA(hOut, s[9], 50, coord, &bytes);
+    coord.Y = 22;
+    WriteConsoleOutputCharacterA(hOut, s[10], 50, coord, &bytes);
+    
+    SetConsoleActiveScreenBuffer(hOut);
+    //fflush(stdin);
+    getch();
+    return ;
+}
+
 
